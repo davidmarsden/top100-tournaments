7b847235ea5ebcfac55a4f2dca89c05ad1a8a53a
davidmarsden <dmarsd@gmail.com>
2026-07-11 20:35:54 +0100

    Route manager portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import AdminDashboard from './components/AdminDashboard.jsx';
 import AdminGate from './components/AdminGate.jsx';
+import ManagerPortal from './components/ManagerPortal.jsx';
 import TournamentRouter, { isAdminPath } from './components/TournamentRouter.jsx';
 import { TournamentProvider } from './context/TournamentProvider.jsx';
 
+function isManagerPath() {
+  return /^\/manager\/?$/.test(window.location.pathname);
+}
+
 export default function App() {
+  if (isManagerPath()) return <ManagerPortal />;
   if (!isAdminPath()) return <TournamentRouter />;
   return <AdminGate><TournamentProvider><AdminDashboard /></TournamentProvider></AdminGate>;
 }
