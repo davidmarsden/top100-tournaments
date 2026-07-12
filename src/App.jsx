4c2be3b911d0176043974294d2346b6bf5d3ec8d
davidmarsden <dmarsd@gmail.com>
2026-07-12 21:49:59 +0100

    Route admin result submissions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AdminDashboard from './components/AdminDashboard.jsx';
 import AdminGate from './components/AdminGate.jsx';
 import ManagerAccountsPage from './components/ManagerAccountsPage.jsx';
 import ManagerPortal from './components/ManagerPortal.jsx';
+import ResultSubmissionsPage from './components/ResultSubmissionsPage.jsx';
 import TournamentRouter, { isAdminPath } from './components/TournamentRouter.jsx';
 import { TournamentProvider } from './context/TournamentProvider.jsx';
 
@@ -13,9 +14,14 @@ function isManagerAccountsPath() {
   return /^\/admin\/manager-accounts\/?$/.test(window.location.pathname);
 }
 
+function isResultSubmissionsPath() {
+  return /^\/admin\/result-submissions\/?$/.test(window.location.pathname);
+}
+
 export default function App() {
   if (isManagerPath()) return <ManagerPortal />;
   if (isManagerAccountsPath()) return <AdminGate><ManagerAccountsPage /></AdminGate>;
+  if (isResultSubmissionsPath()) return <AdminGate><ResultSubmissionsPage /></AdminGate>;
   if (!isAdminPath()) return <TournamentRouter />;
   return <AdminGate><TournamentProvider><AdminDashboard /></TournamentProvider></AdminGate>;
 }
